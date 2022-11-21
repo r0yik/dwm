f7e8a359cbf5752eba754120a606a9b2ceb61049
royik <ryoheiikezawa@protonmail.com>
2022-11-21 16:30:20 +0900

    window title color

--- config.def.h
+++ config.def.h
@@ -26,6 +26,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeTitle]  = { col_gray4, col_gray2,  col_gray2  },
 };
 
 static const char *const autostart[] = {
