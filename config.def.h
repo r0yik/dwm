6326401eb86a14d0fca8a7d97baa64cd63905fcd
royik <ryoheiikezawa@protonmail.com>
2022-11-21 17:27:58 +0900

    changed colorscheme

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -17,16 +17,17 @@ static const char *fonts[]          = { "JetBrains Mono Medium:size=8",
                                         "JoyPixels:pixelsize=9:antialias=true:autohint=true", 
                                         "FiraCode Nerd Font:size=9:antialias=true:autohint=true" };
 
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeTitle]  = { col_gray4, col_gray2,  col_gray2  },
+static const char bg_1[]            = "#0f0f0f";
+static const char fg_1[]            = "#cccccc";
+static const char fg_2[]            = "#aa0000";
+static const char fg_3[]            = "#f0f0f0";
+static const char fg_4[]            = "#444444";
+
+static const char *colors[][3]       = {
+	/*                      fg          bg          border   */
+	[SchemeNorm] =      {   fg_1,       bg_1,       fg_4  },
+	[SchemeSel]  =      {   fg_2,       bg_1,       fg_2  },
+	[SchemeTitle]  =    {   fg_3,       bg_1,       fg_4  },
 };
 
 static const char *const autostart[] = {
